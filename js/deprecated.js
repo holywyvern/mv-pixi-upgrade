3cef17f12c6e0b69e79851c565546390b82424fe
holywyvern <ramiro.rojo@hotmail.es>
2015-10-24 23:53:23 -0300

    Added core files without modifications

---
+++ js/deprecated.js
@@ -0,0 +1,5 @@
+/**
+ * This files aims to count as deprecated any function used by RPG Objects
+ * you should still support it, but add a console.warn("Thing is deprecated.")
+ * So code won't break, and newer code may omit this file.
+ */
